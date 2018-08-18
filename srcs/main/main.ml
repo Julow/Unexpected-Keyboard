3844698b4057d91f624dc95de9c42232d37554dc
juloo <juloo.dsi@gmail.com>
2018-08-18 16:06:05 +0200

    Implement key repeat, quick and dirty

--- srcs/main/main.ml
+++ srcs/main/main.ml
@@ -52,6 +52,7 @@ type t = {
 	modifiers		: Modifiers.t;
 	ims				: Input_method_service.t;
 	view			: View.t;
+	key_repeat		: (Key.t * bool ref) list;
 	dp				: float -> float;
 }
 
@@ -75,12 +76,41 @@ let create ~ims ~view ~dp () = {
 	touch_state = Touch_event.empty_state;
 	layout = Layouts.qwerty;
 	modifiers = Modifiers.empty;
+	key_repeat = [];
 	ims; view; dp
 }
+
+let start_repeat ?(timeout=1000L) key action t =
+	let task t = Keyboard_service.Task t in
+	let enabled = ref true in
+	let rec repeat_loop ~timeout () =
+		let do_repeat () t =
+			if !enabled
+			then t, [ task (repeat_loop ~timeout:200L); action ]
+			else t, []
+		in
+		Lwt.map do_repeat (Keyboard_service.timeout timeout)
+	in
+	let key_repeat = (key, enabled) :: t.key_repeat in
+	{ t with key_repeat }, [ task (repeat_loop ~timeout) ]
+
+let rec stop_repeat key = function
+	| (k', enabled) :: tl when k' == key ->
+		enabled := false;
+		stop_repeat key tl
+	| hd :: tl	-> hd :: stop_repeat key tl
+	| []		-> []
+
+let stop_repeat key =
+	let do_remove t =
+		{ t with key_repeat = stop_repeat key t.key_repeat }, []
+	in
+	Keyboard_service.Task (fun () -> Lwt.return do_remove)
 
 let handle_down t key = function
 	| Key.Modifier m	->
 		{ t with modifiers = Modifiers.on_down m t.modifiers }, []
+	| Typing tv			-> start_repeat key (send tv) t
 	| Change_pad pad	->
 		let layout = match pad with
 			| Default	-> Layouts.qwerty
@@ -94,13 +124,14 @@ let handle_down t key = function
 let handle_cancel t key = function
 	| Key.Modifier m	->
 		{ t with modifiers = Modifiers.on_cancel m t.modifiers }, []
+	| Typing tv			-> t, [ stop_repeat key ]
 	| _					-> t, []
 
 let handle_up t key = function
 	| Key.Typing tv			->
 		let tv = Modifiers.apply tv t.modifiers
 		and modifiers = Modifiers.on_key_press t.modifiers in
-		{ t with modifiers }, [ send tv ]
+		{ t with modifiers }, [ stop_repeat key; send tv ]
 	| Modifier m			->
 		{ t with modifiers = Modifiers.on_up m t.modifiers }, []
 	| _						-> t, []
