539c7fcd7442a3da77bd61d7662b38f879ae4651
Jules Aguillon <juloo.dsi@gmail.com>
2021-01-04 23:31:04 +0100

    Add 'shell.nix' and build the project
    Add a working environment and specify Java version

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {
+    config.android_sdk.accept_license = true;
+    config.allowUnfree = true;
+  } }:
+
+let
+
+  android = pkgs.androidenv.composeAndroidPackages {
+    platformVersions = [ "23" ];
+    abiVersions = [ "armeabi-v7a" ];
+  };
+
+in
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    ant openjdk8 android.androidsdk
+  ];
+  shellHook = ''
+    export ANDROID_HOME=${android.androidsdk}/libexec/android-sdk
+  '';
+}
