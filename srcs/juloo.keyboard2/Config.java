Julow/Unexpected-Keyboard

commit 31d6a70dfb9c7ad3a72302bca2339f926e4593ee

Refactor: Remove KeyValue.name
This makes KeyValue objects smaller. 'equals' and 'hashCode' are now
implemented too. Key names are still used to recognise keys with special
meaning, but not for comparing keys anymore.

srcs/juloo.keyboard2/Config.java
@@ -47,7 +47,7 @@ final class Config
   public String actionLabel; // Might be 'null'
   public int actionId; // Meaningful only when 'actionLabel' isn't 'null'
   public boolean swapEnterActionKey; // Swap the "enter" and "action" keys
-  public Set<String> extra_keys; // 'null' means all the keys
+  public Set<KeyValue> extra_keys; // 'null' means all the keys
 
   public final IKeyEventHandler handler;
 
@@ -151,18 +151,18 @@ final class Config
   {
     // Update the name to avoid caching in KeyModifier
     final KeyValue action_key = (actionLabel == null) ? null :
-      KeyValue.getKeyByName("action").withNameAndSymbol(actionLabel, actionLabel);
+      KeyValue.getKeyByName("action").withSymbol(actionLabel);
     // Extra keys are removed from the set as they are encountered during the
     // first iteration then automatically added.
-    final Set<String> extra_keys = new HashSet<String>(this.extra_keys);
+    final Set<KeyValue> extra_keys = new HashSet<KeyValue>(this.extra_keys);
     KeyboardData kw = original_kw.mapKeys(new KeyboardData.MapKeyValues() {
       public KeyValue apply(KeyValue key)
       {
         if (key == null)
           return null;
-        boolean is_extra_key = extra_keys.contains(key.name);
+        boolean is_extra_key = extra_keys.contains(key);
         if (is_extra_key)
-          extra_keys.remove(key.name);
+          extra_keys.remove(key);
         int flags = key.getFlags();
         if ((flags & KeyValue.FLAG_LOCALIZED) != 0 && !is_extra_key)
           return null;
@@ -196,15 +196,7 @@ final class Config
       }
     });
     if (extra_keys.size() > 0)
-    {
-      final Iterator<String> extra_keys_it = extra_keys.iterator();
-      kw = kw.addExtraKeys(
-          new Iterator<KeyValue>()
-          {
-            public boolean hasNext() { return extra_keys_it.hasNext(); }
-            public KeyValue next() { return KeyValue.getKeyByName(extra_keys_it.next()); }
-          });
-    }
+      kw = kw.addExtraKeys(extra_keys.iterator());
     return kw;
   }
 

srcs/juloo.keyboard2/KeyValue.java
@@ -78,7 +78,6 @@ final class KeyValue
     check((FLAGS_BITS | KIND_BITS | VALUE_BITS) == ~0); // No holes
   }
 
-  public final String name;
   private final String _symbol;
 
   /** This field encodes three things:
@@ -150,37 +149,49 @@ final class KeyValue
   /* Update the char and the symbol. */
   public KeyValue withChar(char c)
   {
-    return new KeyValue(name, String.valueOf(c), KIND_CHAR, c, getFlags());
+    return new KeyValue(String.valueOf(c), KIND_CHAR, c, getFlags());
   }
 
   public KeyValue withString(String s)
   {
-    return new KeyValue(name, s, KIND_STRING, 0, getFlags());
+    return new KeyValue(s, KIND_STRING, 0, getFlags());
   }
 
-  public KeyValue withNameAndSymbol(String n, String s)
+  public KeyValue withSymbol(String s)
   {
-    return new KeyValue(n, s, (_code & KIND_BITS), (_code & VALUE_BITS), getFlags());
+    return new KeyValue(s, (_code & KIND_BITS), (_code & VALUE_BITS), getFlags());
   }
 
   public KeyValue withKeyevent(int code)
   {
-    return new KeyValue(name, _symbol, KIND_KEYEVENT, code, getFlags());
+    return new KeyValue(_symbol, KIND_KEYEVENT, code, getFlags());
   }
 
   public KeyValue withFlags(int f)
   {
-    return new KeyValue(name, _symbol, (_code & KIND_BITS), (_code & VALUE_BITS), f);
+    return new KeyValue(_symbol, (_code & KIND_BITS), (_code & VALUE_BITS), f);
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    KeyValue snd = (KeyValue)obj;
+    return _symbol.equals(snd._symbol) && _code == snd._code;
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return _symbol.hashCode() + _code;
   }
 
   private static HashMap<String, KeyValue> keys = new HashMap<String, KeyValue>();
 
-  public KeyValue(String n, String s, int kind, int value, int flags)
+  public KeyValue(String s, int kind, int value, int flags)
   {
     check((kind & ~KIND_BITS) == 0);
     check((flags & ~FLAGS_BITS) == 0);
     check((value & ~VALUE_BITS) == 0);
-    name = n;
     _symbol = s;
     _code = kind | flags | value;
   }
@@ -207,14 +218,14 @@ final class KeyValue
       return kv.withFlags(kv.getFlags() | FLAG_LOCALIZED);
     }
     if (name.length() == 1)
-      return new KeyValue(name, name, KIND_CHAR, name.charAt(0), 0);
+      return new KeyValue(name, KIND_CHAR, name.charAt(0), 0);
     else
-      return new KeyValue(name, name, KIND_STRING, 0, 0);
+      return new KeyValue(name, KIND_STRING, 0, 0);
   }
 
   private static void addKey(String name, String symbol, int kind, int code, int flags)
   {
-    keys.put(name, new KeyValue(name, symbol, kind, code, flags));
+    keys.put(name, new KeyValue(symbol, kind, code, flags));
   }
 
   private static void addCharKey(String name, String symbol, char c, int flags)
@@ -236,6 +247,11 @@ final class KeyValue
   private static void addKeyeventKey(String name, String symbol, int code, int flags)
   {
     addKey(name, symbol, KIND_KEYEVENT, code, flags);
+  }
+
+  private static void addPlaceholderKey(String name)
+  {
+    addKey(name, "", KIND_STRING, 0, 0);
   }
 
   static
@@ -307,7 +323,9 @@ final class KeyValue
     addCharKey("space", "\r", ' ', FLAG_KEY_FONT);
     addCharKey("nbsp", "\u237d", '\u00a0', FLAG_KEY_FONT | FLAG_SMALLER_FONT);
 
-    addKey("removed", "", KIND_STRING, 0, 0); // Dummy key used in [KeyModifier]
+    addPlaceholderKey("removed");
+    addPlaceholderKey("f11_placeholder");
+    addPlaceholderKey("f12_placeholder");
   }
 
   // Substitute for [assert], which has no effect on Android.

srcs/juloo.keyboard2/Keyboard2.java
@@ -80,19 +80,19 @@ public class Keyboard2 extends InputMethodService
     _currentTextLayout = l;
   }
 
-  private void extra_keys_of_subtype(Set<String> dst, InputMethodSubtype subtype)
+  private void extra_keys_of_subtype(Set<KeyValue> dst, InputMethodSubtype subtype)
   {
     String extra_keys = subtype.getExtraValueOf("extra_keys");
     if (extra_keys == null)
       return;
     String[] ks = extra_keys.split("\\|");
     for (int i = 0; i < ks.length; i++)
-      dst.add(ks[i]);
+      dst.add(KeyValue.getKeyByName(ks[i]));
   }
 
   private void refreshAccentsOption(InputMethodManager imm, InputMethodSubtype subtype)
   {
-    HashSet<String> extra_keys = new HashSet<String>();
+    HashSet<KeyValue> extra_keys = new HashSet<KeyValue>();
     List<InputMethodSubtype> enabled_subtypes = getEnabledSubtypes(imm);
     switch (_config.accents)
     {
@@ -119,7 +119,7 @@ public class Keyboard2 extends InputMethodService
     switch (_config.accents)
     {
       case 1: case 2: case 3: _config.extra_keys = null; break;
-      case 4: _config.extra_keys = new HashSet<String>(); break;
+      case 4: _config.extra_keys = new HashSet<KeyValue>(); break;
     }
     // Fallback for the layout option: Use qwerty in the "system settings" case
     _currentTextLayout = (_config.layout == -1) ? R.xml.qwerty : _config.layout;

srcs/juloo.keyboard2/Pointers.java
@@ -68,11 +68,8 @@ public final class Pointers implements Handler.Callback
    */
   public int getKeyFlags(KeyValue kv)
   {
-    // Comparing names because the keys might have been modified.
-    // Physical equality works because names are never computed or shared.
-    String name = kv.name;
     for (Pointer p : _ptrs)
-      if (p.value != null && p.value.name == name)
+      if (p.value != null && p.value.equals(kv))
         return p.flags;
     return -1;
   }
@@ -214,7 +211,7 @@ public final class Pointers implements Handler.Callback
     {
       ptr.selected_direction = direction;
       KeyValue newValue = getKeyAtDirection(ptr, direction);
-      if (newValue != null && (ptr.value == null || newValue.name != ptr.value.name))
+      if (newValue != null && (ptr.value == null || !newValue.equals(ptr.value)))
       {
         int old_flags = ptr.flags;
         ptr.value = newValue;
@@ -253,7 +250,7 @@ public final class Pointers implements Handler.Callback
     if (v == null)
       return null;
     for (Pointer p : _ptrs)
-      if (p.key == k && p.pointerId == -1 && p.value != null && p.value.name == v.name)
+      if (p.key == k && p.pointerId == -1 && p.value != null && p.value.equals(v))
         return p;
     return null;
   }

srcs/juloo.keyboard2/Emoji.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 
 public class Emoji
 {
+  private final String _name;
   private final KeyValue _kv;
   private final String _desc;
 
@@ -17,14 +18,15 @@ public class Emoji
 
   protected Emoji(String name, String bytecode, String desc)
   {
-    _kv = new KeyValue(name, bytecode, KeyValue.KIND_STRING, 0, 0);
+    _name = name;
+    _kv = new KeyValue(bytecode, KeyValue.KIND_STRING, 0, 0);
     _desc = desc;
     emojis_by_name.put(name, this);
   }
 
   public String name()
   {
-    return _kv.name;
+    return _name;
   }
 
   public KeyValue kv()

commit ab987c776c7ddc0fab594f20f4ddf49d98848ccd

Fix localized key not in predefined position
The "loc " prefix for predefining a place for an "extra key" was broken
since 31d6a70.

The FLAG_LOCALIZED flag cannot be used anymore, as adding it to any key
would turn it into a different key that wouldn't be recognized by parts
of the code comparing the keys (placing the extra keys).

Add an other layer in KeyboardData to store such informations.

srcs/juloo.keyboard2/Config.java
@@ -160,15 +160,12 @@ final class Config
     // first iteration then automatically added.
     final Set<KeyValue> extra_keys = new HashSet<KeyValue>(this.extra_keys);
     KeyboardData kw = original_kw.mapKeys(new KeyboardData.MapKeyValues() {
-      public KeyValue apply(KeyValue key)
+      public KeyValue apply(KeyValue key, boolean localized)
       {
-        if (key == null)
-          return null;
         boolean is_extra_key = extra_keys.contains(key);
         if (is_extra_key)
           extra_keys.remove(key);
-        int flags = key.getFlags();
-        if ((flags & KeyValue.FLAG_LOCALIZED) != 0 && !is_extra_key)
+        if (localized && !is_extra_key)
           return null;
         switch (key.getKind())
         {
@@ -193,7 +190,7 @@ final class Config
             break;
           case Modifier:
             if (lockable_modifiers.contains(key.getModifier()))
-              return key.withFlags(flags | KeyValue.FLAG_LOCK);
+              return key.withFlags(key.getFlags() | KeyValue.FLAG_LOCK);
             break;
         }
         return key;

srcs/juloo.keyboard2/EmojiKeyButton.java
@@ -14,7 +14,8 @@ public class EmojiKeyButton extends Button
   {
     super(context, attrs);
     setOnClickListener(this);
-    _key = KeyValue.getKeyByName(attrs.getAttributeValue(null, "key"));
+    String key_name = attrs.getAttributeValue(null, "key");
+    _key = (key_name == null) ? null : KeyValue.getKeyByName(key_name);
     setText(_key.getString());
     if (_key.hasFlags(KeyValue.FLAG_KEY_FONT))
       setTypeface(Theme.getSpecialKeyFont(context));

srcs/juloo.keyboard2/KeyValue.java
@@ -58,8 +58,6 @@ final class KeyValue
   public static final int FLAG_SMALLER_FONT = (1 << 25);
   // Used by [Pointers].
   public static final int FLAG_LOCKED = (1 << 26);
-  // Language specific keys that are removed from the keyboard by default.
-  public static final int FLAG_LOCALIZED = (1 << 27);
 
   // Kinds
   public static final int KIND_CHAR = (0 << 29);
@@ -196,27 +194,11 @@ final class KeyValue
     _code = kind | flags | value;
   }
 
-  private static String stripPrefix(String s, String prefix)
-  {
-    if (s.startsWith(prefix))
-      return s.substring(prefix.length());
-    else
-      return null;
-  }
-
   public static KeyValue getKeyByName(String name)
   {
-    if (name == null)
-      return null;
-    KeyValue kv = KeyValue.keys.get(name);
+    KeyValue kv = keys.get(name);
     if (kv != null)
       return kv;
-    String localized = stripPrefix(name, "loc ");
-    if (localized != null)
-    {
-      kv = getKeyByName(localized);
-      return kv.withFlags(kv.getFlags() | FLAG_LOCALIZED);
-    }
     if (name.length() == 1)
       return new KeyValue(name, KIND_CHAR, name.charAt(0), 0);
     else

srcs/juloo.keyboard2/Keyboard2.java
@@ -296,10 +296,9 @@ public class Keyboard2 extends InputMethodService
         return;
       KeyboardData layout =
         getLayout(_config.programming_layout).mapKeys(new KeyboardData.MapKeyValues() {
-          public KeyValue apply(KeyValue key)
+          public KeyValue apply(KeyValue key, boolean localized)
           {
-            if (key != null
-                && key.getKind() == KeyValue.Kind.Event
+            if (key.getKind() == KeyValue.Kind.Event
                 && key.getEvent() == KeyValue.Event.SWITCH_PROGRAMMING)
               return KeyValue.getKeyByName("switch_text");
             return key;

srcs/juloo.keyboard2/Keyboard2View.java
@@ -266,26 +266,30 @@ public class Keyboard2View extends View
     return defaultColor;
   }
 
-  private void drawLabel(Canvas canvas, KeyValue k, float x, float y, float keyH, boolean isKeyDown)
+  private void drawLabel(Canvas canvas, KeyboardData.Corner k, float x, float y, float keyH, boolean isKeyDown)
   {
-    k = KeyModifier.modify(k, _mods);
     if (k == null)
       return;
-    float textSize = scaleTextSize(k, _config.labelTextSize, keyH);
-    Paint p = _theme.labelPaint(k.hasFlags(KeyValue.FLAG_KEY_FONT));
-    p.setColor(labelColor(k, isKeyDown, _theme.labelColor));
+    KeyValue kv = KeyModifier.modify(k.kv, _mods);
+    if (kv == null)
+      return;
+    float textSize = scaleTextSize(kv, _config.labelTextSize, keyH);
+    Paint p = _theme.labelPaint(kv.hasFlags(KeyValue.FLAG_KEY_FONT));
+    p.setColor(labelColor(kv, isKeyDown, _theme.labelColor));
     p.setTextSize(textSize);
-    canvas.drawText(k.getString(), x, (keyH - p.ascent() - p.descent()) / 2f + y, p);
+    canvas.drawText(kv.getString(), x, (keyH - p.ascent() - p.descent()) / 2f + y, p);
   }
 
-  private void drawSubLabel(Canvas canvas, KeyValue k, float x, float y, float keyW, float keyH, Paint.Align a, Vertical v, boolean isKeyDown)
+  private void drawSubLabel(Canvas canvas, KeyboardData.Corner k, float x, float y, float keyW, float keyH, Paint.Align a, Vertical v, boolean isKeyDown)
   {
-    k = KeyModifier.modify(k, _mods);
     if (k == null)
       return;
-    float textSize = scaleTextSize(k, _config.sublabelTextSize, keyH);
-    Paint p = _theme.subLabelPaint(k.hasFlags(KeyValue.FLAG_KEY_FONT), a);
-    p.setColor(labelColor(k, isKeyDown, _theme.subLabelColor));
+    KeyValue kv = KeyModifier.modify(k.kv, _mods);
+    if (kv == null)
+      return;
+    float textSize = scaleTextSize(kv, _config.sublabelTextSize, keyH);
+    Paint p = _theme.subLabelPaint(kv.hasFlags(KeyValue.FLAG_KEY_FONT), a);
+    p.setColor(labelColor(kv, isKeyDown, _theme.subLabelColor));
     p.setTextSize(textSize);
     float subPadding = _config.keyPadding;
     if (v == Vertical.CENTER)
@@ -296,7 +300,7 @@ public class Keyboard2View extends View
       x += keyW / 2f;
     else
       x += (a == Paint.Align.LEFT) ? subPadding : keyW - subPadding;
-    canvas.drawText(k.getString(), x, y, p);
+    canvas.drawText(kv.getString(), x, y, p);
   }
 
   private float scaleTextSize(KeyValue k, float rel_size, float keyH)

srcs/juloo.keyboard2/KeyboardData.java
@@ -177,11 +177,11 @@ class KeyboardData
      **   0
      ** 3   4
      */
-    public final KeyValue key0;
-    public final KeyValue key1;
-    public final KeyValue key2;
-    public final KeyValue key3;
-    public final KeyValue key4;
+    public final Corner key0;
+    public final Corner key1;
+    public final Corner key2;
+    public final Corner key3;
+    public final Corner key4;
 
     /** Key width in relative unit. */
     public final float width;
@@ -190,7 +190,7 @@ class KeyboardData
     /** Put keys 1 to 4 on the edges instead of the corners. */
     public final boolean edgekeys;
 
-    protected Key(KeyValue k0, KeyValue k1, KeyValue k2, KeyValue k3, KeyValue k4, float w, float s, boolean e)
+    protected Key(Corner k0, Corner k1, Corner k2, Corner k3, Corner k4, float w, float s, boolean e)
     {
       key0 = k0;
       key1 = k1;
@@ -204,11 +204,11 @@ class KeyboardData
 
     public static Key parse(XmlResourceParser parser) throws Exception
     {
-      KeyValue k0 = KeyValue.getKeyByName(parser.getAttributeValue(null, "key0"));
-      KeyValue k1 = KeyValue.getKeyByName(parser.getAttributeValue(null, "key1"));
-      KeyValue k2 = KeyValue.getKeyByName(parser.getAttributeValue(null, "key2"));
-      KeyValue k3 = KeyValue.getKeyByName(parser.getAttributeValue(null, "key3"));
-      KeyValue k4 = KeyValue.getKeyByName(parser.getAttributeValue(null, "key4"));
+      Corner k0 = Corner.parse_of_attr(parser, "key0");
+      Corner k1 = Corner.parse_of_attr(parser, "key1");
+      Corner k2 = Corner.parse_of_attr(parser, "key2");
+      Corner k3 = Corner.parse_of_attr(parser, "key3");
+      Corner k4 = Corner.parse_of_attr(parser, "key4");
       float width = parser.getAttributeFloatValue(null, "width", 1f);
       float shift = parser.getAttributeFloatValue(null, "shift", 0.f);
       boolean edgekeys = parser.getAttributeBooleanValue(null, "edgekeys", false);
@@ -225,27 +225,30 @@ class KeyboardData
 
     public KeyValue getKeyValue(int i)
     {
+      Corner c;
       switch (i)
       {
-        case 0: return key0;
-        case 1: return key1;
-        case 2: return key2;
-        case 3: return key3;
-        case 4: return key4;
-        default: return null;
+        case 0: c = key0; break;
+        case 1: c = key1; break;
+        case 2: c = key2; break;
+        case 3: c = key3; break;
+        case 4: c = key4; break;
+        default: c = null; break;
       }
+      return (c == null) ? null : c.kv;
     }
 
     public Key withKeyValue(int i, KeyValue kv)
     {
-      KeyValue k0 = key0, k1 = key1, k2 = key2, k3 = key3, k4 = key4;
+      Corner k0 = key0, k1 = key1, k2 = key2, k3 = key3, k4 = key4;
+      Corner k = Corner.of_kv(kv);
       switch (i)
       {
-        case 0: k0 = kv; break;
-        case 1: k1 = kv; break;
-        case 2: k2 = kv; break;
-        case 3: k3 = kv; break;
-        case 4: k4 = kv; break;
+        case 0: k0 = k; break;
+        case 1: k1 = k; break;
+        case 2: k2 = k; break;
+        case 3: k3 = k; break;
+        case 4: k4 = k; break;
       }
       return new Key(k0, k1, k2, k3, k4, width, shift, edgekeys);
     }
@@ -255,6 +258,7 @@ class KeyboardData
      */
     public KeyValue getAtDirection(int direction)
     {
+      Corner c = null;
       if (edgekeys)
       {
         // \ 1 /
@@ -264,10 +268,10 @@ class KeyboardData
         // / 4 \
         switch (direction)
         {
-          case 2: case 3: return key1;
-          case 4: case 5: return key2;
-          case 6: case 7: return key4;
-          case 8: case 1: return key3;
+          case 2: case 3: c = key1; break;
+          case 4: case 5: c = key2; break;
+          case 6: case 7: c = key4; break;
+          case 8: case 1: c = key3; break;
         }
       }
       else
@@ -279,13 +283,56 @@ class KeyboardData
         // 3 | 4
         switch (direction)
         {
-          case 1: case 2: return key1;
-          case 3: case 4: return key2;
-          case 5: case 6: return key4;
-          case 7: case 8: return key3;
+          case 1: case 2: c = key1; break;
+          case 3: case 4: c = key2; break;
+          case 5: case 6: c = key4; break;
+          case 7: case 8: c = key3; break;
         }
       }
-      return null;
+      return (c == null) ? null : c.kv;
+    }
+  }
+
+  public static final class Corner
+  {
+    public final KeyValue kv;
+    /** Whether the kv is marked with the "loc " prefix. To be removed if not
+        specified in the [extra_keys]. */
+    public final boolean localized;
+
+    protected Corner(KeyValue k, boolean l)
+    {
+      kv = k;
+      localized = l;
+    }
+
+    public static Corner parse_of_attr(XmlResourceParser parser, String attr) throws Exception
+    {
+      String name = parser.getAttributeValue(null, attr);
+      boolean localized = false;
+
+      if (name == null)
+        return null;
+      String name_loc = stripPrefix(name, "loc ");
+      if (name_loc != null)
+      {
+        localized = true;
+        name = name_loc;
+      }
+      return new Corner(KeyValue.getKeyByName(name), localized);
+    }
+
+    public static Corner of_kv(KeyValue kv)
+    {
+      return new Corner(kv, false);
+    }
+
+    private static String stripPrefix(String s, String prefix)
+    {
+      if (s.startsWith(prefix))
+        return s.substring(prefix.length());
+      else
+        return null;
     }
   }
 
@@ -295,12 +342,22 @@ class KeyboardData
   }
 
   public static abstract class MapKeyValues implements MapKey {
-    abstract public KeyValue apply(KeyValue kv);
+    abstract public KeyValue apply(KeyValue c, boolean localized);
 
     public Key apply(Key k)
     {
       return new Key(apply(k.key0), apply(k.key1), apply(k.key2),
           apply(k.key3), apply(k.key4), k.width, k.shift, k.edgekeys);
+    }
+
+    private Corner apply(Corner c)
+    {
+      if (c == null)
+        return null;
+      KeyValue kv = apply(c.kv, c.localized);
+      if (kv == null)
+        return null;
+      return Corner.of_kv(kv);
     }
   }
 

srcs/juloo.keyboard2/Pointers.java
@@ -140,7 +140,7 @@ public final class Pointers implements Handler.Callback
     // Don't take latched modifiers into account if an other key is pressed.
     // The other key already "own" the latched modifiers and will clear them.
     Modifiers mods = getModifiers(isOtherPointerDown());
-    KeyValue value = _handler.modifyKey(key.key0, mods);
+    KeyValue value = handleKV(key.key0, mods);
     Pointer ptr = new Pointer(pointerId, key, value, x, y, mods);
     _ptrs.add(ptr);
     if (value != null && !value.hasFlags(KeyValue.FLAG_SPECIAL))
@@ -159,7 +159,7 @@ public final class Pointers implements Handler.Callback
   private KeyValue getKeyAtDirection(Pointer ptr, int direction)
   {
     if (direction == 0)
-      return _handler.modifyKey(ptr.key.key0, ptr.modifiers);
+      return handleKV(ptr.key.key0, ptr.modifiers);
     KeyValue k;
     for (int i = 0; i > -3; i = (~i>>31) - i)
     {
@@ -171,6 +171,13 @@ public final class Pointers implements Handler.Callback
         return k;
     }
     return null;
+  }
+
+  private KeyValue handleKV(KeyboardData.Corner c, Modifiers modifiers)
+  {
+    if (c == null)
+      return null;
+    return _handler.modifyKey(c.kv, modifiers);
   }
 
   public void onTouchMove(float x, float y, int pointerId)