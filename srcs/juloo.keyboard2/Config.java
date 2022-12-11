8665d002bf8a6c7fda7ea337115f30d05fcd0beb
Chasm Solacer <46286197+ChasmSolacer@users.noreply.github.com>
2022-12-11 14:49:22 +0100

    Added qwerty_pl layout (#251)
    * Updated pl translation
    
    * Added qwerty_pl.xml – QWERTY (Polski) layout

--- srcs/juloo.keyboard2/Config.java
+++ srcs/juloo.keyboard2/Config.java
@@ -297,6 +297,7 @@ final class Config
       case "qwerty_pt": id = R.xml.qwerty_pt; break;
       case "qwerty_sv_se": id = R.xml.qwerty_sv_se; break;
       case "qwerty_tr": id = R.xml.qwerty_tr; break;
+      case "qwerty_pl": id = R.xml.qwerty_pl; break;
       case "qwertz": id = R.xml.qwertz; break;
       case "qwertz_cs": id = R.xml.qwertz_cs; break;
       case "qwertz_de": id = R.xml.qwertz_de; break;
