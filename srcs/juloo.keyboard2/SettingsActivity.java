68d54479b82a6870de1389a46af25044ead58d28
RetrogisusDEV <99091223+RetrogisusDEV@users.noreply.github.com>
2024-01-19 10:57:06 -0400

    updated SettingsActivity.java
    fix error in theme in settingsActivity

--- srcs/juloo.keyboard2/SettingsActivity.java
+++ srcs/juloo.keyboard2/SettingsActivity.java
@@ -26,25 +26,7 @@ public class SettingsActivity extends PreferenceActivity
     addPreferencesFromResource(R.xml.settings);
   }
 
-  /** The default theme is [Theme.DeviceDefault], which is dark. Detect if the
-      system is using light theme. */
-  void detectSystemTheme()
-  {
-    if (Build.VERSION.SDK_INT >= 14)
-    {
-      int ui_mode = getResources().getConfiguration().uiMode;
-      if ((ui_mode & Configuration.UI_MODE_NIGHT_NO) != 0)
-        setTheme(android.R.style.Theme_DeviceDefault_Light);
-    }
-    if (Build.VERSION.SDK_INT >= 21)
-    {
-      int ui_mode = getResources().getConfiguration().uiMode;
-      if ((ui_mode & Configuration.UI_MODE_NIGHT_NO) != 0)
-        setTheme(R.style.appTheme);
-      else
-        setTheme(R.style.appTheme_Night);
-    }
-  }
+  
 
   void fallbackEncrypted()
   {
