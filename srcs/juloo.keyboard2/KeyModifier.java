Julow/Unexpected-Keyboard

commit 3f6b6fd23253e5326d9ba29cac10be36305b3019

refactor: Apply the modmap in KeyModifier
This makes possible to apply other modifiers to mapped keys and ensures
that the modmap really override the shift key behavior.

srcs/juloo.keyboard2/KeyModifier.java
@@ -10,6 +10,11 @@ public final class KeyModifier
   private static HashMap<KeyValue, HashMap<Pointers.Modifiers, KeyValue>> _cache =
     new HashMap<KeyValue, HashMap<Pointers.Modifiers, KeyValue>>();
 
+  /** The optional modmap takes priority over modifiers usual behaviors. Set to
+      [null] to disable. */
+  private static KeyboardData.Modmap _modmap = null;
+  public static void set_modmap(KeyboardData.Modmap mm) { _modmap = mm; }
+
   /** Modify a key according to modifiers. */
   public static KeyValue modify(KeyValue k, Pointers.Modifiers mods)
   {
@@ -130,6 +135,12 @@ public final class KeyModifier
 
   private static KeyValue apply_shift(KeyValue k)
   {
+    if (_modmap != null)
+    {
+      KeyValue mapped = _modmap.shift.get(k);
+      if (mapped != null)
+        return mapped;
+    }
     switch (k.getKind())
     {
       case Char:

srcs/juloo.keyboard2/Keyboard2View.java
@@ -104,6 +104,7 @@ public class Keyboard2View extends View
     }
     _compose_kv = KeyValue.getKeyByName("compose");
     _compose_key = _keyboard.findKeyWithValue(_compose_kv);
+    KeyModifier.set_modmap(_keyboard.modmap);
     reset();
   }
 
@@ -137,15 +138,6 @@ public class Keyboard2View extends View
 
   public KeyValue modifyKey(KeyValue k, Pointers.Modifiers mods)
   {
-    if (_keyboard.modmap != null)
-    {
-      if (mods.has(KeyValue.Modifier.SHIFT))
-      {
-        KeyValue km = _keyboard.modmap.shift.get(k);
-        if (km != null)
-          return km;
-      }
-    }
     return KeyModifier.modify(k, mods);
   }
 

commit c7ed2bcae915886b9d35bd19934f40816ef86b81

Clear cache when changing layout
Due to the modmap, the cache must be cleared when switching layout to or
from a layout that contain a modmap.

This is broken since 3f6b6fd23, which moves the modmap handling into
KeyModifier.

srcs/juloo.keyboard2/KeyModifier.java
@@ -13,7 +13,14 @@ public final class KeyModifier
   /** The optional modmap takes priority over modifiers usual behaviors. Set to
       [null] to disable. */
   private static KeyboardData.Modmap _modmap = null;
-  public static void set_modmap(KeyboardData.Modmap mm) { _modmap = mm; }
+  public static void set_modmap(KeyboardData.Modmap mm)
+  {
+    // Clear the cache when switching to or from a layout that contain a modmap
+    // as the cache would otherwise override the modmap.
+    if (_modmap != null || mm != null)
+      _cache.clear();
+    _modmap = mm;
+  }
 
   /** Modify a key according to modifiers. */
   public static KeyValue modify(KeyValue k, Pointers.Modifiers mods)