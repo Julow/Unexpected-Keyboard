84e10e047012f55d6c2ba2a2c53da93cc9ac00e4
Reza Hosseinzadeh <69734726+RZHSSNZDH@users.noreply.github.com>
2024-07-21 11:37:01 +0000

    Improve zwnj/halfspace symbol and Persian layout (#711)
    * Fix persian's half-space invisibility
    * Add a 0.5 space before ظ in persian
    * Change the place of چ in persian
    * Change the width of backspace in persian
    * Rename zwnj to halfspace in KeyValue.java and beng_provat layout

--- srcs/juloo.keyboard2/KeyValue.java
+++ srcs/juloo.keyboard2/KeyValue.java
@@ -550,7 +550,8 @@ public final class KeyValue implements Comparable<KeyValue>
       case "meteg_placeholder": return placeholderKey(Placeholder.METEG);
       /* intending/preventing ligature - supported by many scripts*/
       case "zwj": return charKey("zwj", '\u200D', 0); // zero-width joiner (provides ligature)
-      case "zwnj": return charKey("zwnj", '\u200C', 0); // zero-width non joiner (prevents unintended ligature)
+      case "zwnj":
+      case "halfspace": return charKey("⸽", '\u200C', 0); // zero-width non joiner
 
       /* Editing keys */
       case "copy": return editingKey(0xE030, Editing.COPY);
