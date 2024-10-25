73742a853f346a39e7b923ba1c0919dc32570190
Kalan Walmsley <97612117+kalankaboom@users.noreply.github.com>
2024-10-25 18:54:25 -0400

    Rosé Pine Theme (#788)

--- srcs/juloo.keyboard2/Config.java
+++ srcs/juloo.keyboard2/Config.java
@@ -421,6 +421,7 @@ public final class Config
         if ((night_mode & Configuration.UI_MODE_NIGHT_NO) != 0)
           return R.style.MonetLight;
         return R.style.MonetDark;
+      case "rosepine": return R.style.RosePine;
       default:
       case "system":
         if ((night_mode & Configuration.UI_MODE_NIGHT_NO) != 0)
