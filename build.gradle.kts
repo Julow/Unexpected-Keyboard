ab99857a72ac3971f557b0f899737778bf016dee
JFronny <git@akbvopenfl1.hopto.org>
2025-09-05 19:49:17 +0200

    Downgrade Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -136,7 +136,7 @@ val compileComposeSequences by tasks.registering(Exec::class) {
   inputs.dir(`in`)
   outputs.file(out)
   doFirst { println("\nGenerating $out") }
-  val sequences = `in`.listFiles {
+  val sequences = `in`.listFiles().filter {
     !it.name.endsWith(".py") && !it.name.endsWith(".md")
   }!!.map { it.absolutePath }.toTypedArray()
   workingDir = projectDir
