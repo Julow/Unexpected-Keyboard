camillanapoles/Unexpected-Keyboard-cnmfs

Track the currently typed word
The `CurrentlyTypedWord` class tracks the word that is being typed. It's
implemented on the same model as Autocapitalisation and avoid expensive
IPC calls when possible.

The `Suggestions` class is where the suggestion lookup should go. It
currently just echoes the current word.

srcs/juloo.keyboard2/CurrentlyTypedWord.java
@@ -1,0 +1,98 @@
+package juloo.keyboard2;
+
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import java.util.List;
+
+/** Keep track of the word being typed. */
+public final class CurrentlyTypedWord
+{
+  InputConnection _ic = null;
+  Callback _callback;
+
+  StringBuilder _w = new StringBuilder();
+  boolean _enabled = false;
+
+  /** The estimated cursor position. Used to avoid expensive IPC calls when the
+      typed word can be estimated locally with [typed]. When the cursor
+      position gets out of sync, the text before the cursor is queried again to
+      the editor. */
+  int _cursor;
+
+  int refresh_count = 0;
+
+  public CurrentlyTypedWord(Callback cb)
+  {
+    _callback = cb;
+  }
+
+  public void started(EditorInfo info, InputConnection ic)
+  {
+    _ic = ic;
+    refresh_current_word(info.getInitialTextBeforeCursor(10, 0),
+        info.initialSelStart != info.initialSelEnd);
+    _cursor = info.initialSelStart;
+  }
+
+  public void typed(String s)
+  {
+    if (!_enabled)
+      return;
+    type_chars(s);
+    callback();
+  }
+
+  public void selection_updated(int oldSelStart, int newSelStart, int newSelEnd)
+  {
+    // Avoid the expensive [refresh_current_word] call when [typed] was called
+    // before.
+    if (!_enabled || newSelStart == _cursor)
+      return;
+    refresh_current_word(_ic.getTextBeforeCursor(10, 0),
+        newSelStart != newSelEnd);
+    _cursor = newSelStart;
+  }
+
+  private void callback()
+  {
+    _callback.currently_typed_word(_w.toString() + refresh_count);
+  }
+
+  /** Estimate the currently typed word after [chars] has been typed. */
+  private void type_chars(String s)
+  {
+    int len = s.length();
+    for (int i = 0; i < len;)
+    {
+      int c = s.codePointAt(i);
+      if (Character.isLetter(c))
+        _w.appendCodePoint(c);
+      else
+        _w.setLength(0);
+      _cursor++;
+      i += Character.charCount(c);
+    }
+  }
+
+  /** Set [_enabled]. */
+  private void refresh_current_word(CharSequence text_before_cursor, boolean has_selection)
+  {
+    _w.setLength(0);
+    if (_ic == null || text_before_cursor == null)
+    {
+      _enabled = false;
+      return;
+    }
+    _enabled = true;
+    if (has_selection)
+      return;
+    refresh_count++;
+    type_chars(text_before_cursor.toString());
+    callback();
+  }
+
+  public static interface Callback
+  {
+    public void currently_typed_word(String word);
+  }
+}

srcs/juloo.keyboard2/KeyEventHandler.java
@@ -14,10 +14,13 @@ import java.util.Iterator;
 
 public final class KeyEventHandler
   implements Config.IKeyEventHandler,
-             ClipboardHistoryService.ClipboardPasteCallback
+             ClipboardHistoryService.ClipboardPasteCallback,
+             CurrentlyTypedWord.Callback
 {
   IReceiver _recv;
   Autocapitalisation _autocap;
+  Suggestions _suggestions;
+  CurrentlyTypedWord _typedword;
   /** State of the system modifiers. It is updated whether a modifier is down
       or up and a corresponding key event is sent. */
   Pointers.Modifiers _mods;
@@ -35,19 +38,24 @@ public final class KeyEventHandler
     _autocap = new Autocapitalisation(recv.getHandler(),
         this.new Autocapitalisation_callback());
     _mods = Pointers.Modifiers.EMPTY;
+    _suggestions = new Suggestions(recv);
+    _typedword = new CurrentlyTypedWord(this);
   }
 
   /** Editing just started. */
   public void started(EditorInfo info)
   {
-    _autocap.started(info, _recv.getCurrentInputConnection());
+    InputConnection ic = _recv.getCurrentInputConnection();
+    _autocap.started(info, ic);
+    _typedword.started(info, ic);
     _move_cursor_force_fallback = should_move_cursor_force_fallback(info);
   }
 
   /** Selection has been updated. */
-  public void selection_updated(int oldSelStart, int newSelStart)
+  public void selection_updated(int oldSelStart, int newSelStart, int newSelEnd)
   {
     _autocap.selection_updated(oldSelStart, newSelStart);
+    _typedword.selection_updated(oldSelStart, newSelStart, newSelEnd);
   }
 
   /** A key is being pressed. There will not necessarily be a corresponding
@@ -121,6 +129,12 @@ public final class KeyEventHandler
   public void paste_from_clipboard_pane(String content)
   {
     send_text(content);
+  }
+
+  @Override
+  public void currently_typed_word(String word)
+  {
+    _suggestions.currently_typed_word(word);
   }
 
   /** Update [_mods] to be consistent with the [mods], sending key events if
@@ -212,13 +226,14 @@ public final class KeyEventHandler
       _autocap.event_sent(eventCode, metaState);
   }
 
-  void send_text(CharSequence text)
+  void send_text(String text)
   {
     InputConnection conn = _recv.getCurrentInputConnection();
     if (conn == null)
       return;
     conn.commitText(text, 1);
     _autocap.typed(text);
+    _typedword.typed(text);
   }
 
   /** See {!InputConnection.performContextMenuAction}. */
@@ -485,7 +500,7 @@ public final class KeyEventHandler
     return info.packageName.startsWith("org.godotengine.editor");
   }
 
-  public static interface IReceiver
+  public static interface IReceiver extends Suggestions.Callback
   {
     public void handle_event_key(KeyValue.Event ev);
     public void set_shift_state(boolean state, boolean lock);

Julow/Unexpected-Keyboard

srcs/juloo.keyboard2/CurrentlyTypedWord.java
@@ -1,0 +1,99 @@
+package juloo.keyboard2;
+
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import java.util.List;
+
+/** Keep track of the word being typed. */
+public final class CurrentlyTypedWord
+{
+  InputConnection _ic = null;
+  Callback _callback;
+
+  StringBuilder _w = new StringBuilder();
+  boolean _enabled = false;
+
+  /** The estimated cursor position. Used to avoid expensive IPC calls when the
+      typed word can be estimated locally with [typed]. When the cursor
+      position gets out of sync, the text before the cursor is queried again to
+      the editor. */
+  int _cursor;
+
+  int refresh_count = 0;
+
+  public CurrentlyTypedWord(Callback cb)
+  {
+    _callback = cb;
+  }
+
+  public void started(Config conf, InputConnection ic)
+  {
+    _ic = ic;
+    EditorConfig e = conf.editor_config;
+    refresh_current_word(e.initial_text_before_cursor,
+        e.initial_sel_start != e.initial_sel_end);
+    _cursor = e.initial_sel_start;
+  }
+
+  public void typed(String s)
+  {
+    if (!_enabled)
+      return;
+    type_chars(s);
+    callback();
+  }
+
+  public void selection_updated(int oldSelStart, int newSelStart, int newSelEnd)
+  {
+    // Avoid the expensive [refresh_current_word] call when [typed] was called
+    // before.
+    if (!_enabled || newSelStart == _cursor)
+      return;
+    refresh_current_word(_ic.getTextBeforeCursor(10, 0),
+        newSelStart != newSelEnd);
+    _cursor = newSelStart;
+  }
+
+  private void callback()
+  {
+    _callback.currently_typed_word(_w.toString() + refresh_count);
+  }
+
+  /** Estimate the currently typed word after [chars] has been typed. */
+  private void type_chars(String s)
+  {
+    int len = s.length();
+    for (int i = 0; i < len;)
+    {
+      int c = s.codePointAt(i);
+      if (Character.isLetter(c))
+        _w.appendCodePoint(c);
+      else
+        _w.setLength(0);
+      _cursor++;
+      i += Character.charCount(c);
+    }
+  }
+
+  /** Set [_enabled]. */
+  private void refresh_current_word(CharSequence text_before_cursor, boolean has_selection)
+  {
+    _w.setLength(0);
+    if (_ic == null || text_before_cursor == null)
+    {
+      _enabled = false;
+      return;
+    }
+    _enabled = true;
+    if (has_selection)
+      return;
+    refresh_count++;
+    type_chars(text_before_cursor.toString());
+    callback();
+  }
+
+  public static interface Callback
+  {
+    public void currently_typed_word(String word);
+  }
+}

srcs/juloo.keyboard2/KeyEventHandler.java
@@ -12,10 +12,13 @@ import java.util.Iterator;
 
 public final class KeyEventHandler
   implements Config.IKeyEventHandler,
-             ClipboardHistoryService.ClipboardPasteCallback
+             ClipboardHistoryService.ClipboardPasteCallback,
+             CurrentlyTypedWord.Callback
 {
   IReceiver _recv;
   Autocapitalisation _autocap;
+  Suggestions _suggestions;
+  CurrentlyTypedWord _typedword;
   /** State of the system modifiers. It is updated whether a modifier is down
       or up and a corresponding key event is sent. */
   Pointers.Modifiers _mods;
@@ -33,20 +36,25 @@ public final class KeyEventHandler
     _autocap = new Autocapitalisation(recv.getHandler(),
         this.new Autocapitalisation_callback());
     _mods = Pointers.Modifiers.EMPTY;
+    _suggestions = new Suggestions(recv);
+    _typedword = new CurrentlyTypedWord(this);
   }
 
   /** Editing just started. */
   public void started(Config conf)
   {
-    _autocap.started(conf, _recv.getCurrentInputConnection());
+    InputConnection ic = _recv.getCurrentInputConnection();
+    _autocap.started(conf, ic);
+    _typedword.started(conf, ic);
     _move_cursor_force_fallback =
       conf.editor_config.should_move_cursor_force_fallback;
   }
 
   /** Selection has been updated. */
-  public void selection_updated(int oldSelStart, int newSelStart)
+  public void selection_updated(int oldSelStart, int newSelStart, int newSelEnd)
   {
     _autocap.selection_updated(oldSelStart, newSelStart);
+    _typedword.selection_updated(oldSelStart, newSelStart, newSelEnd);
   }
 
   /** A key is being pressed. There will not necessarily be a corresponding
@@ -120,6 +128,12 @@ public final class KeyEventHandler
   public void paste_from_clipboard_pane(String content)
   {
     send_text(content);
+  }
+
+  @Override
+  public void currently_typed_word(String word)
+  {
+    _suggestions.currently_typed_word(word);
   }
 
   /** Update [_mods] to be consistent with the [mods], sending key events if
@@ -211,13 +225,14 @@ public final class KeyEventHandler
       _autocap.event_sent(eventCode, metaState);
   }
 
-  void send_text(CharSequence text)
+  void send_text(String text)
   {
     InputConnection conn = _recv.getCurrentInputConnection();
     if (conn == null)
       return;
     conn.commitText(text, 1);
     _autocap.typed(text);
+    _typedword.typed(text);
   }
 
   /** See {!InputConnection.performContextMenuAction}. */
@@ -473,7 +488,7 @@ public final class KeyEventHandler
     return (conn.getSelectedText(0) != null);
   }
 
-  public static interface IReceiver
+  public static interface IReceiver extends Suggestions.Callback
   {
     public void handle_event_key(KeyValue.Event ev);
     public void set_shift_state(boolean state, boolean lock);