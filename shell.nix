4406c2a6e1111f1824e395d2a532bda4db9c119f
Jure Varlec <jure@varlec.si>
2026-04-01 20:52:39 +0200

    shell.nix: make gradlew work on NixOS
    This removes the wrapper provided by `shell.nix`. Folks expect the
    `./gradlew` wrapper to work, so this approach is more straightforward.

--- shell.nix
+++ shell.nix
@@ -36,26 +36,16 @@ let
 
   ANDROID_SDK_ROOT = "${android.androidsdk}/libexec/android-sdk";
 
-  gradle = pkgs.gradle.override { java = jdk; };
-  # Without this option, aapt2 fails to run with a permissions error.
-  gradle_wrapped = pkgs.runCommandLocal "gradle-wrapped" {
-    nativeBuildInputs = with pkgs; [ makeBinaryWrapper ];
-  } ''
-    mkdir -p $out/bin
-    ln -s ${gradle}/bin/gradle $out/bin/gradle
-    wrapProgram $out/bin/gradle \
-    --add-flags "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${build_tools_version}/aapt2"
-  '';
-
 in pkgs.mkShell {
   buildInputs = [
     pkgs.findutils
     pkgs.fontforge
     jdk
     android.androidsdk
-    gradle_wrapped
+    (pkgs.gradle.override { java = jdk; })
     emulators
   ];
   JAVA_HOME = jdk.home;
   inherit ANDROID_SDK_ROOT;
+  GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${build_tools_version}/aapt2";
 }
